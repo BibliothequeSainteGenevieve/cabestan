16e772c5d53aaa0fea391f84d79a6512454456af
anne-line <anne-line.colombo@thetribe.io>
2025-02-27 17:19:33 +0100

    fix: no header link onto homepage

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,19 +1,32 @@
 import { useTranslation } from "react-i18next";
 import logo from "../assets/logo-bsg.png";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
+import { routes } from "@/pages/routes";
 
 export default function Header() {
 	const { t } = useTranslation();
+	const location = useLocation();
 
 	return (
 		<header className="pb-4 border-b border-border">
-			<Link to="/" className="flex items-center gap-3">
-				<img src={logo} alt="Cabestan" width={50} height={50} />
-				<div className="flex flex-col gap-1">
-					<p className="text-2xl">{t("Header.title")}</p>
-					<p className="text-sm text-grey">{t("Header.subtitle")}</p>
+			{location.pathname !== routes.home.path && (
+				<Link to={routes.home.path} className="flex items-center gap-3">
+					<img src={logo} alt="Cabestan" width={50} height={50} />
+					<div className="flex flex-col gap-1">
+						<p className="text-2xl">{t("Header.title")}</p>
+						<p className="text-sm text-grey">{t("Header.subtitle")}</p>
+					</div>
+				</Link>
+			)}
+			{location.pathname === routes.home.path && (
+				<div className="flex items-center gap-3">
+					<img src={logo} alt="Cabestan" width={50} height={50} />
+					<div className="flex flex-col gap-1">
+						<p className="text-2xl">{t("Header.title")}</p>
+						<p className="text-sm text-grey">{t("Header.subtitle")}</p>
+					</div>
 				</div>
-			</Link>
+			)}
 		</header>
 	);
 }
