b3319c7ff527e2d2cc09397eb7815c8867ff7699
anne-line <anne-line.colombo@thetribe.io>
2025-03-03 11:17:32 +0100

    fix: Conditionally apply capitalize class in search input

--- client/src/components/SearchBar/SearchInput/index.tsx
+++ client/src/components/SearchBar/SearchInput/index.tsx
@@ -58,7 +58,7 @@ export function CommandSearch({ isBookSearch }: CommandSearchProps) {
 					placeholder={isBookSearch ? t("CommandSearch.placeholderBook") : t("CommandSearch.placeholder")}
 					value={inputValue}
 					onChange={(event) => handleChange(event.target.value)}
-					className="border-none shadow-none capitalize"
+					className={`border-none shadow-none ${inputValue ? "capitalize" : ""}`}
 				/>
 			</div>
 			{isOpen && (
