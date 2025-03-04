686d3a236c56a062d2415884fbe510d3d8e2d813
anne-line <anne-line.colombo@thetribe.io>
2025-03-04 11:50:29 +0100

    feat: Add export functionality for RCR documents results

--- client/src/components/DocumentsExport.tsx
+++ client/src/components/DocumentsExport.tsx
@@ -1,19 +1,22 @@
 import { Download } from "lucide-react";
 import { useTranslation } from "react-i18next";
 import { Button } from "@/components/ui/button";
-import { getRCRExportCSV } from "@/api";
-import { useSearchParams } from "react-router";
+import { getDocumentsExportCSV } from "@/api";
+import { useParams, useSearchParams } from "react-router";
 
-export default function RCRExport() {
+export default function DocumentsExport() {
 	const { t } = useTranslation();
+	const { rcr } = useParams();
 	const [searchParams] = useSearchParams();
+
+	if (!rcr) return null;
 
 	return (
 		<div className="flex gap-2 print:hidden">
 			<Button
 				variant="foreground"
 				onClick={() => {
-					getRCRExportCSV(searchParams.toString());
+					getDocumentsExportCSV(rcr, searchParams.toString());
 				}}>
 				<Download className="w-4 h-4" />
 				{t("Export.csv")}
